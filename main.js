1a1d8726bb2877e198a9c804b929f1f96b07f11d
Jonathan Rodriguez <clubzspadez@gmail.com>
2025-01-12 19:23:11 -0500

    site update

---
+++ main.js
@@ -0,0 +1,6 @@
+function closeModal() {
+  document.getElementById('directionsModal').classList.remove('active');
+}
+function openModal() {
+  document.getElementById('directionsModal').classList.add('active');
+}
